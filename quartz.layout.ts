7bca7f5db86e55dd7311506316671018ea60da32
utilitariant <utilitariant@gmail.com>
2025-12-02 19:02:21 -0700

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,13 +33,13 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Flex({
       components: [
         {
-          Component: Component.Search(),
+              Component: Component.Darkmode(),
+              Component: Component.ReaderMode(),
           grow: true,
         },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
       ],
     }),
+    Component: Component.Search(),
     Component.Explorer(),
   ],
   right: [
