130b904ed7813e06d578a3b755fb7ee31cd1cd7a
utilitariant <utilitariant@gmail.com>
2025-12-02 19:06:51 -0700

    Revert "Update quartz.layout.ts"
    This reverts commit cb8ffda6102dd445044c6e930de8c36e1d578df6.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -28,8 +28,6 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component: Component.Darkmode()
-    Component: Component.ReaderMode()
     Component.Flex({
       components: [
         {
